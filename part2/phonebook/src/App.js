1da965ae708a02152e121786da691da685b2ff7a
Suraj Jadhav <jadhavsuraj777@gmail.com>
2021-08-16 16:03:03 +0530

    part2: 2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,10 +5,11 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
   const addNewPerson = e => {
     e.preventDefault();
     if (persons.findIndex(p => p.name===newName) === -1) {
-      setPersons(persons.concat({ name: newName }));
+      setPersons(persons.concat({ name: newName, number: newNumber }));
     } else {
       alert(`${newName} is already added to phonebook`);
     }
@@ -22,11 +23,14 @@ const App = () => {
           name: <input value={newName} onChange={e => setNewName(e.target.value)}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange={e => setNewNumber(e.target.value)}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
 }
