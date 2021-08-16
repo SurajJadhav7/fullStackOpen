54eab63027e509140aec5c8790b512368183fb54
Suraj Jadhav <jadhavsuraj777@gmail.com>
2021-08-16 11:43:54 +0530

    part2: 2.3*: Course information step8

--- part2/course-info/src/Total.js
+++ part2/course-info/src/Total.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Total = ({ parts }) => {
+    
     var total = parts.reduce((acc, part) => acc+part.exercises, 0);
+
     return (
         <strong>total of {total} exercises</strong>
     );
