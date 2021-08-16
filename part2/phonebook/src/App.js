83071234722edff2f5d1fe43402f20d13e588bd0
Suraj Jadhav <jadhavsuraj777@gmail.com>
2021-08-16 16:32:09 +0530

    part2: 2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ filter, setFilter ] = useState('')
   const addNewPerson = e => {
     e.preventDefault();
     if (persons.findIndex(p => p.name===newName) === -1) {
@@ -14,10 +18,18 @@ const App = () => {
       alert(`${newName} is already added to phonebook`);
     }
   }
+  const search = e => {
+    e.preventDefault();
+    setFilter(e.target.value);
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with<input value={filter} onChange={search} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addNewPerson}>
         <div>
           name: <input value={newName} onChange={e => setNewName(e.target.value)}/>
@@ -30,7 +42,12 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+      {!filter ?
+        persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)
+     :
+      persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+        .map(person => <p key={person.name}>{person.name} {person.number}</p>)
+      }
     </div>
   )
 }
