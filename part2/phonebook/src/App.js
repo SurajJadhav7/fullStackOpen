0e337da42128806cf43d81007c961742f630a458
Suraj Jadhav <jadhavsuraj777@gmail.com>
2021-08-17 11:16:01 +0530

    part2: 2.16: Phonebook step8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import personsService from './services/persons';
 
 const App = () => {
   const [ persons, setPersons ] = useState([]) 
@@ -10,17 +10,17 @@ const App = () => {
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter ] = useState('')
   useEffect(() => {
-    axios.get('http://localhost:3001/persons')
-      .then(response => setPersons(response.data))
+    personsService.getAll()
+      .then(response => setPersons(response))
       .catch(error => console.log(error))
   }, []);
   const addNewPerson = e => {
     e.preventDefault();
     if (persons.findIndex(p => p.name===newName) === -1) {
       const newPerson = { name: newName, number: newNumber };
-      axios.post('http://localhost:3001/persons', newPerson)
+      personsService.addNew(newPerson)
         .then(response => {
-          setPersons(persons.concat(response.data));
+          setPersons(persons.concat(response));
         })
         .catch(error => console.log(error))
     } else {
---
+++ part2/phonebook/src/services/persons.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3001/persons";
+
+const getAll = () => {
+    const promise = axios.get(baseUrl);
+    return promise.then(response => response.data);
+};
+
+const addNew = person => {
+    const promise = axios.post(baseUrl, person);
+    return promise.then(response => response.data);
+}
+
+export default { getAll, addNew };
